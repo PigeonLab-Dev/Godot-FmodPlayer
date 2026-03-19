18002074d8c7c5fc4f26d421d921b87863f9d63c
LuYingYiLong <l_y_y_l@icloud.com>
2026-03-19 23:34:52 +0800

    fix

--- src/core/register_types.cpp
+++ src/core/register_types.cpp
@@ -54,7 +54,7 @@ using namespace godot;
 
 static FmodServer* fmod_server_instance = nullptr;
 
-void initialize_example_module(ModuleInitializationLevel p_level) {
+void initialize_fmod_player_module(ModuleInitializationLevel p_level) {
 	if (p_level == MODULE_INITIALIZATION_LEVEL_EDITOR) {
 		// 注册自定义项目设置
 		ProjectSettings* settings = ProjectSettings::get_singleton();
@@ -170,7 +170,7 @@ void initialize_example_module(ModuleInitializationLevel p_level) {
 	Engine::get_singleton()->register_singleton("FmodServer", FmodServer::get_singleton());
 }
 
-void uninitialize_example_module(ModuleInitializationLevel p_level) {
+void uninitialize_fmod_player_module(ModuleInitializationLevel p_level) {
 	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
 		return;
 	}
@@ -186,8 +186,8 @@ extern "C" {
 	GDExtensionBool GDE_EXPORT fmod_player_init(GDExtensionInterfaceGetProcAddress p_get_proc_address, const GDExtensionClassLibraryPtr p_library, GDExtensionInitialization* r_initialization) {
 		godot::GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
 
-		init_obj.register_initializer(initialize_example_module);
-		init_obj.register_terminator(uninitialize_example_module);
+		init_obj.register_initializer(initialize_fmod_player_module);
+		init_obj.register_terminator(uninitialize_fmod_player_module);
 		init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_EDITOR);
 
 		return init_obj.init();
